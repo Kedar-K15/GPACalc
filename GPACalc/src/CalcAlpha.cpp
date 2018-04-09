1d3d62ca2580793de3460b2cdaaf8dd91837e0ff
Kedar7833 <Kedar7833@users.noreply.github.com>
2018-04-08 20:29:39 -0400

    Add license notices
    The license notices follow under GNU General Public License v2.0

--- GPACalc/src/CalcAlpha.cpp
+++ GPACalc/src/CalcAlpha.cpp
@@ -1,11 +1,27 @@
+/**
+GPACalc: A program that calculates one's Grade Point Average
+Copyright (C) 2018  Kedar Kulkarni
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**/
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <stdexcept>
 #include "CalcAlpha.h"
 #include "CalcMain.h"
-
-/* All rights to Kedar Kulkarni */
 
 bool CalcAlpha::in_map(const std::string& grade, const std::unordered_map<std::string,float>& given) {
     std::unordered_map<std::string,float>::const_iterator found = given.find(grade);
